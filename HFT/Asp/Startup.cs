14fae266f2a865b51eceb67eae45b1ab33e93a6b
tsaskater <42976086+tsaskater@users.noreply.github.com>
2020-11-03 12:14:57 +0100

    Create Startup.cs

---
+++ HFT/Asp/Startup.cs
@@ -0,0 +1,37 @@
+using asp05.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace asp05
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(opt => opt.EnableEndpointRouting = false);
+            services.AddDbContext<PersonContext>(opt =>
+            {
+                opt.UseLazyLoadingProxies();
+                opt.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=pregisterdb;Trusted_Connection=True;MultipleActiveResultSets=True");
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseMvcWithDefaultRoute();
+
+            app.UseRouting();
+        }
+    }
+}
